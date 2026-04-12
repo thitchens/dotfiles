019bac928f5b0af5a17ebdbc4543004827f2e35f
thitchens <thomasjhitchens@gmail.com>
2026-04-12 16:41:45 -0700

    move Readme, add git window keymap

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -3,5 +3,6 @@ return {
 	dependencies = { "airblade/vim-gitgutter" },
 	keys = {
 		{ "<leader>gb", ":Git blame<CR>", desc = "Blame Line" },
+		{ "<leader>gg", ":Git<CR>", desc = "Open git window" },
 	},
 }
