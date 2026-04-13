c8c64870abad92c339dc17bd4dcb50625bee1d07
thitchens <thomasjhitchens@gmail.com>
2026-04-12 19:56:28 -0700

    Swap to main branch of treesitter

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,27 +1,22 @@
 return {
-	-- https://github.com/nvim-treesitter/nvim-treesitter
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
+	branch = "main",
+	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "terraform", "go", "python", "rust", "yaml" },
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
+		require("nvim-treesitter.config").setup({
+			--ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "terraform", "go", "rust", "yaml" },
 			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 			auto_install = true,
-
-			highlight = {
-				enable = true,
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
+			highlight = { enable = true },
 		})
+		-- Replacing ensure_installed functionality
+		local ensureInstalled = { "lua", "vim", "vimdoc", "query", "terraform", "go", "rust", "yaml" }
+		local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+		local parsersToInstall = vim.iter(ensureInstalled)
+			:filter(function(parser)
+				return not vim.tbl_contains(alreadyInstalled, parser)
+			end)
+			:totable()
+		require("nvim-treesitter").install(parsersToInstall)
 	end,
 }
