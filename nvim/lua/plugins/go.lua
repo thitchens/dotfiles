8428f55bd6c6233f45fc0db35f16be31619b0cf4
thitchens <thitchens@terasense.net>
2026-04-14 16:17:23 -0700

    tidy up go and tree sitter

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,21 +1,23 @@
 return {
-	"ray-x/go.nvim",
-	dependencies = {
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
-		"akinsho/toggleterm.nvim",
-	},
-	config = function()
-		require("go").setup()
-	end,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-	keys = {
-		{ "<leader>of", "<cmd>GoFillStruct<cr>", desc = ":GoFillStruct" },
-		{ "<leader>ot", "<cmd>GoModTidy<cr>", desc = ":GoModTidy" },
-		{ "<leader>or", '<cmd>TermExec cmd="go run ."<cr>', desc = "go run terminal" },
-		{ "<leader>oa", "<cmd>GoAddTag<cr>", desc = ":GoAddTag" },
-	},
+    "ray-x/go.nvim",
+    dependencies = {
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+        "akinsho/toggleterm.nvim",
+    },
+    config = function()
+        require("go").setup({
+            lsp_codelens = false,
+        })
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    keys = {
+        { "<leader>of", "<cmd>GoFillStruct<cr>",            desc = ":GoFillStruct" },
+        { "<leader>ot", "<cmd>GoModTidy<cr>",               desc = ":GoModTidy" },
+        { "<leader>or", '<cmd>TermExec cmd="go run ."<cr>', desc = "go run terminal" },
+        { "<leader>oa", "<cmd>GoAddTag<cr>",                desc = ":GoAddTag" },
+    },
 }
--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,22 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
+	branch = "master",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.config").setup({
-			--ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "terraform", "go", "rust", "yaml" },
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "terraform", "go", "rust", "yaml" },
 			sync_install = false,
 			auto_install = true,
 			highlight = { enable = true },
 		})
-		-- Replacing ensure_installed functionality
-		local ensureInstalled = { "lua", "vim", "vimdoc", "query", "terraform", "go", "rust", "yaml" }
-		local alreadyInstalled = require("nvim-treesitter.config").get_installed()
-		local parsersToInstall = vim.iter(ensureInstalled)
-			:filter(function(parser)
-				return not vim.tbl_contains(alreadyInstalled, parser)
-			end)
-			:totable()
-		require("nvim-treesitter").install(parsersToInstall)
 	end,
 }
